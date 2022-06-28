820f31052bac27d679b6b8219572373957afe46c
lehtojaa <jaakko.lehto@gispo.fi>
2022-06-28 14:45:58 +0300

    Add new help menus

--- rasterriskassessmentplugin/core/hri_model.py
+++ rasterriskassessmentplugin/core/hri_model.py
@@ -24,62 +24,30 @@ class NaturalHazardRisksForSchools(CombineRasters):
     @classmethod
     def shortHelpString(cls):  # noqa: N802
         return """<html><body><h2>Algorithm description</h2>
-<p>This algorithm creates a composite hazard risk index by using anywhere from 2 to 6 different raster hazard risk layers. The user can define custom weights (default is 1) for each raster. The result include a raster </p>
+<p>This algorithm creates a composite hazard risk index by using anywhere from 2 to 6 different raster hazard risk layers. The user can define custom weights for each raster. The result includes a raster ranging from 1 to 4, where 1 is Lower risk and 4 is Higher risk, plus a sampled point layer for schools with the corresponding risk index.</p>
 <h2>Input parameters</h2>
 <h3>Number of risks to compute</h3>
 <p>Any whole number between 2 and 6</p>
-<h3>Hazard layer 1</h3>
-<p></p>
-<h3>Weight for Hazard layer 1</h3>
-<p></p>
-<h3>Hazard layer 2</h3>
-<p></p>
-<h3>Weight for Hazard layer 2</h3>
-<p></p>
-<h3>Hazard layer 3</h3>
-<p></p>
-<h3>Weight for Hazard layer 3</h3>
-<p></p>
-<h3>Hazard layer 4</h3>
-<p></p>
-<h3>Weight for Hazard layer 4</h3>
-<p></p>
-<h3>Hazard layer 5</h3>
-<p></p>
-<h3>Weight for Hazard layer 5</h3>
-<p></p>
-<h3>Hazard layer 6</h3>
-<p></p>
-<h3>Weight for Hazard layer 6</h3>
-<p></p>
-<h3>Projected Reference System</h3>
-<p></p>
+<h3>Hazard layers</h3>
+<p>All hazard layers can have any range (e.g., 1 to 100, 0 to 5, etc.), as long as the classification runs from less risk (lower values) to higher risk (higher values). If this is not the case, the user should begin by multiplying the raster layer by -1 before running this process. </p>
+<h3>Weight for Hazard layers</h3>
+<p>Please make sure that all weights add up to 100%</p>
 <h3>Schools</h3>
 <p>A point vector layer with one observation for each school in the area of analysis</p>
 <h3>Study area</h3>
 <p>A polygon vector layer delimiting the area of analysis.</p>
 <h3>Hazard index</h3>
-<p></p>
-<h3>Layer 1</h3>
-<p></p>
-<h3>Layer 2</h3>
-<p></p>
-<h3>Layer 3</h3>
-<p></p>
+<p>Raster layer ranging from 1 (Less risky) to 4 (More risky)</p>
 <h3>Hazard Index - Schools</h3>
-<p>The original point layer file with the information on the composite hazard index that corresponds to the location of the school</p>
-<h2>Outputs</h2>
+<p>The original point layer file with the information on the composite hazard index that corresponds to the location of the school, ranging from 1 (Less risky) to 4 (More risky).</p>
+<h3>Projected reference system</h3>
+<p>Select a local projected reference system. Note that failing to do so might lead to inaccuracies when calculations are performed far from the equator. </p><h2>Outputs</h2>
 <h3>Hazard index</h3>
-<p></p>
-<h3>Layer 1</h3>
-<p></p>
-<h3>Layer 2</h3>
-<p></p>
-<h3>Layer 3</h3>
-<p></p>
+<p>Raster layer ranging from 1 (Less risky) to 4 (More risky)</p>
 <h3>Hazard Index - Schools</h3>
-<p>The original point layer file with the information on the composite hazard index that corresponds to the location of the school</p>
-<br><p align="right">Algorithm author: Development unit at IIEP-UNESCO (development@iiep.unesco.org)</p><p align="right">Help author: Development unit at IIEP-UNESCO (development@iiep.unesco.org)</p><p align="right">Algorithm version: 1.0</p></body></html>"""  # noqa
+<p>The original point layer file with the information on the composite hazard index that corresponds to the location of the school, ranging from 1 (Less risky) to 4 (More risky).</p>
+<br><p>Algorithm author: Development unit at IIEP-UNESCO (development@iiep.unesco.org)</p><p>Help author: Development unit at IIEP-UNESCO (development@iiep.unesco.org)</p><p>Algorithm version: 1.0</p></body></html>
+"""
 
     def helpUrl(self):  # noqa: N802
         return (
